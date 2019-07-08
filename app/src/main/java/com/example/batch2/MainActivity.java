d8218950c6deecd914e27d3de58df2640672d0ae
abdul <abdul.tanveer@gmail.com>
2019-07-08 20:38:29 +0530

    logcat, activity lifecycle

--- app/src/main/java/com/example/batch2/MainActivity.java
+++ app/src/main/java/com/example/batch2/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.provider.AlarmClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     public  static String KEY = "mykey";
+    public  static String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,6 +20,44 @@ public class MainActivity extends AppCompatActivity {
         /*Button loginButton = new Button(this);
         loginButton.setText("login");
         loginButton.setWidth(100);*/
+        Toast.makeText(this, "created - m/y allocated", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "started -- visible", Toast.LENGTH_SHORT).show();
+        Log.d(TAG,"onStart"+"interacting user");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "paused -- partially visible", Toast.LENGTH_SHORT).show();
+        Log.i(TAG,"onPause"+"save game state");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "resumed", Toast.LENGTH_SHORT).show();
+        Log.w(TAG,"onResume"+"restore game state");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "stopped", Toast.LENGTH_SHORT).show();
+        Log.e(TAG,"onStop"+"release resources");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "destroyed", Toast.LENGTH_SHORT).show();
 
     }
 

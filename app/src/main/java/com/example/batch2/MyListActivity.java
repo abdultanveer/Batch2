8b63df82cd9df113627e28bb21da349a93ae6804
abdul <abdul.tanveer@gmail.com>
2019-07-10 21:20:05 +0530

    options menu

--- app/src/main/java/com/example/batch2/MyListActivity.java
+++ app/src/main/java/com/example/batch2/MyListActivity.java
@@ -1,7 +1,11 @@
 package com.example.batch2;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -31,8 +35,44 @@ public class MyListActivity extends AppCompatActivity implements AdapterView.OnI
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+         super.onCreateOptionsMenu(menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.mylist_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+         super.onOptionsItemSelected(item);
+         switch (item.getItemId()){
+             case R.id.deleteMenuitem:
+                 Toast.makeText(this, "deleting", Toast.LENGTH_SHORT).show();
+                 break;
+             case R.id.editMenuitem:
+                 Toast.makeText(this, "editing", Toast.LENGTH_SHORT).show();
+
+                 break;
+         }
+
+        return true;
+    }
+
+    @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         String item = parent.getItemAtPosition(position).toString();
         Toast.makeText(this, item, Toast.LENGTH_SHORT).show();
+    }
+
+    public void handleClick(View view) {
+        showAlertDialog();
+    }
+
+    private void showAlertDialog() {
+        AlertDialog.Builder alertDialog = new
+                AlertDialog.Builder(this);
+        alertDialog.setTitle("title of alert dialog");
+        alertDialog.setMessage("alert message");
+        alertDialog.show();
     }
 }
